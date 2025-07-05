4bb644fd08e9b8135aad1e603a802a870d9267c3
saito-simaru <22311088ks@tama.ac.jp>
2025-07-05 11:48:08 +0900

    cssを追加し、画像とテキストをスマホの画面に合わせて表示させる

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # HTMLテンプレートを返す
+    return render_template('index.html') #htmlファイルの表示
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
