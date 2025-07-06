70f61a39a8f630c927aa78f08fbf4ae80cc3e932
saito-simaru <22311088ks@tama.ac.jp>
2025-07-06 17:07:41 +0900

    外部スクリプトから送られた画像を更新しながら表示

---
+++ image_generator.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageDraw
+import time
+
+def update_loop():
+    while True:
+        img = Image.new('RGB', (400, 300), color='black')
+        d = ImageDraw.Draw(img)
+        current_time = time.strftime("%H:%M:%S")
+        d.text((10, 10), f"Now: {current_time}", fill='white')
+        img.save('static/images/generated_image.jpg')
+        print("画像更新:", current_time)
+        time.sleep(15)  # 5秒おきに画像更新
+
+if __name__ == '__main__':
+    update_loop()
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') #htmlファイルの表示
+    # 画像ファイル名を設定（必要に応じて変更）
+    image_filename = 'generated_image.jpg'
+    image_path = os.path.join('images', image_filename).replace('\\', '/')
+    
+    print("image_path:", image_path)
+    
+    return render_template('index.html', image_path=image_path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
